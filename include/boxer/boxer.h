93ff8741edf4a0647b1f0df45ed0a25540519ba6
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-12-19 12:33:38 -0800

    Expand show method

--- include/boxer/boxer.h
+++ include/boxer/boxer.h
@@ -3,7 +3,43 @@
 
 namespace boxer {
 
-void show(const char *message, const char *title);
+enum class Style {
+   Info,
+   Warning,
+   Error,
+   Question
+};
+
+enum class Buttons {
+   OK,
+   OKCancel,
+   YesNo
+};
+
+enum class Selection {
+   OK,
+   Cancel,
+   Yes,
+   No,
+   None
+};
+
+const Style DEFAULT_STYLE = Style::Info;
+const Buttons DEFAULT_BUTTONS = Buttons::OK;
+
+Selection show(const char *message, const char *title, Style style, Buttons buttons);
+
+inline Selection show(const char *message, const char *title, Style style) {
+   return show(message, title, style, DEFAULT_BUTTONS);
+}
+
+inline Selection show(const char *message, const char *title, Buttons buttons) {
+   return show(message, title, DEFAULT_STYLE, buttons);
+}
+
+inline Selection show(const char *message, const char *title) {
+   return show(message, title, DEFAULT_STYLE, DEFAULT_BUTTONS);
+}
 
 } // namespace boxer
 
