040f3107969278307179d57d7d91638dcadb76a1
Aaron Jacobs <aaron@aaronmjacobs.com>
2014-12-19 12:34:44 -0800

    Implement OS X expanded show

--- lib/boxer_osx.mm
+++ lib/boxer_osx.mm
@@ -3,17 +3,88 @@
 
 namespace boxer {
 
-void show(const char *message, const char *title) {
+namespace {
+
+NSString* const OK_STR = @"OK";
+NSString* const CANCEL_STR = @"Cancel";
+NSString* const YES_STR = @"Yes";
+NSString* const NO_STR = @"No";
+
+NSAlertStyle getAlertStyle(Style style) {
+   switch (style) {
+      case Style::Info:
+         return NSInformationalAlertStyle;
+      case Style::Warning:
+         return NSWarningAlertStyle;
+      case Style::Error:
+         return NSCriticalAlertStyle;
+      case Style::Question:
+         return NSWarningAlertStyle;
+      default:
+         return NSInformationalAlertStyle;
+   }
+}
+
+void setButtons(NSAlert *alert, Buttons buttons) {
+   switch (buttons) {
+      case Buttons::OK:
+         [alert addButtonWithTitle:OK_STR];
+         break;
+      case Buttons::OKCancel:
+         [alert addButtonWithTitle:OK_STR];
+         [alert addButtonWithTitle:CANCEL_STR];
+         break;
+      case Buttons::YesNo:
+         [alert addButtonWithTitle:YES_STR];
+         [alert addButtonWithTitle:NO_STR];
+         break;
+      default:
+         [alert addButtonWithTitle:OK_STR];
+   }
+}
+
+Selection getSelection(int index, Buttons buttons) {
+   switch (buttons) {
+      case Buttons::OK:
+         return index == NSAlertFirstButtonReturn ? Selection::OK : Selection::None;
+      case Buttons::OKCancel:
+         if (index == NSAlertFirstButtonReturn) {
+            return Selection::OK;
+         } else if (index == NSAlertSecondButtonReturn) {
+            return Selection::Cancel;
+         } else {
+            return Selection::None;
+         }
+      case Buttons::YesNo:
+         if (index == NSAlertFirstButtonReturn) {
+            return Selection::Yes;
+         } else if (index == NSAlertSecondButtonReturn) {
+            return Selection::No;
+         } else {
+            return Selection::None;
+         }
+      default:
+         return Selection::None;
+   }
+}
+
+} // namespace
+
+Selection show(const char *message, const char *title, Style style, Buttons buttons) {
    NSAlert *alert = [[NSAlert alloc] init];
-   [alert addButtonWithTitle:@"OK"];
+
    [alert setMessageText:[NSString stringWithCString:title
                                    encoding:[NSString defaultCStringEncoding]]];
    [alert setInformativeText:[NSString stringWithCString:message
                                        encoding:[NSString defaultCStringEncoding]]];
-   [alert setAlertStyle:NSWarningAlertStyle];
 
-   [alert runModal];
+   [alert setAlertStyle:getAlertStyle(style)];
+   setButtons(alert, buttons);
+
+   Selection selection = getSelection([alert runModal], buttons);
    [alert release];
+
+   return selection;
 }
 
 } // namespace boxer
