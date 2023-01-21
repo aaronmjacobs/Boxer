cd86b81e9834903862883cc2ebc43db971f7e9ec
Aaron Jacobs <aaron@aaronmjacobs.com>
2023-01-21 13:37:37 -0800

    Examples style

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,14 +1,16 @@
 #include <boxer/boxer.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[])
+{
    boxer::show("Simple message boxes are very easy to create.", "Simple Example");
 
    boxer::show("There are a few different message box styles to choose from.", "Style Example", boxer::Style::Error);
 
-   boxer::Selection selection;
-   do {
+   boxer::Selection selection = boxer::Selection::Yes;
+   while (selection == boxer::Selection::Yes)
+   {
       selection = boxer::show("Different buttons may be used, and the user's selection can be checked. Would you like to see this message again?", "Selection Example", boxer::Style::Question, boxer::Buttons::YesNo);
-   } while (selection == boxer::Selection::Yes);
+   }
 
    return 0;
 }
